a5be27ef4441347d7c37acce3792503931c48085
Luis Tapia <devlou@protonmail.com>
2022-10-26 17:32:59 -0500

    Add font styles and prices with Intl

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const baseUrl = "https://platzi-avo.vercel.app";
 const appNode = document.querySelector("#app");
 
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+  return newPrice;
+};
+
 // fetch web api
 // connect to server
 // TODO: replace promises with async await
@@ -21,11 +29,18 @@ window
       const title = document.createElement("h2");
       document.body.appendChild(title);
       title.textContent = item.name;
+      // title.style = "font-size: 3rem";
+      // title.style.fontSize = "3rem";
+      title.className = "text-2xl text-red-600";
 
       // create price
+      // Intl
+      // - format dates
+      // - format currencies
       const price = document.createElement("div");
-      price.textContent = item.price;
+      price.textContent = formatPrice(item.price);
 
+      // TODO: make the site pretty with CSS
       const container = document.createElement("div");
       container.append(image, title, price);
 
