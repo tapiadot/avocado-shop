c9d98da6be198f3a90d959ff25f62d01ed861f12
Luis Tapia <devlou@protonmail.com>
2022-10-26 16:17:30 -0500

    Download project info and create nodes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const url = "https://platzi-avo.vercel.app/api/avo";
 
-console.log('Happy hacking :)')
+// fetch web api
+// connect to server
+// TODO: replace promises with async await
+window
+  .fetch(url)
+  // process response and convert to JSON
+  .then((response) => response.json())
+  // JSON -> Data -> Render info browser
+  .then((responseJson) => {
+    const allItems = [];
+    responseJson.data.forEach((item) => {
+      // create image
+      const image = document.createElement("img");
+      document.body.appendChild(image);
+      // create title
+      const title = document.createElement("h2");
+      document.body.appendChild(title);
+      // create price
+      const price = document.createElement("div");
+      document.body.appendChild(price);
+
+      const container = document.createElement("div");
+      container.append(image, title, price);
+
+      allItems.push(container);
+    });
+
+    document.body.append(...allItems);
+  });
